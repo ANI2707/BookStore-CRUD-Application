f399db5d51106feed9ab39089e5dfcfbbf2682d6
Aniket <108017771+ANI2707@users.noreply.github.com>
2024-04-06 16:13:02 +0530

    Update DeleteBook.jsx

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`http://localhost:5555/books/${id}`)
+      .delete(`https://book-store-crud-application-eosin.vercel.app/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });        navigate('/');
